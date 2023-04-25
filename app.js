00ecb8988513623873357d4754354f82177f45c7
kenanbylan <kenan.baylan4654@gmail.com>
2023-04-25 23:06:09 +0300

    ı write open api swagger documention

--- app.js
+++ app.js
@@ -12,26 +12,12 @@ const port = process.env.PORT || 8080;
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger-output.json");
 
-const options = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "My API",
-      version: "1.0.0",
-      description: "My API description",
-    },
-  },
-
-  // apis: ["./routes/*.js"], // API yollarınızın yolunu belirtin
-  apis: ["./routers/*.js", "./routers/**/*.js"],
-};
-
 //saat dilimi ayarları tam olarak 3 saat geriden geliyor. Bu yüzden 3 saat ekledim.
 momentTimezone.tz.setDefault("Europe/Istanbul"); //Tarih ve saat işlemleri için kullanılır.
 
 //Cors İmplementing
-const cors = require("cors");
-const corsSettings = require("./src/Helper/corsSettings");
+//const cors = require("cors");
+//const corsSettings = require("./src/Helper/corsSettings");
 
 const errorHandlerMiddleware = require("./src/middleware/errorHandler");
 
@@ -47,8 +33,8 @@ app.use(express.static(path.dirname(__dirname, "/public")));
 app.use("/public", express.static(__dirname));
 
 //Cors ayarları implemente edildi.
-app.use(cors(corsSettings)); //Cors ayarları implemente edildi.
-app.use(cors()); //Cors ayarları implemente edildi. (Bu şekilde de kullanılabilir.
+//app.use(cors(corsSettings)); //Cors ayarları implemente edildi.
+//app.use(cors()); //Cors ayarları implemente edildi. (Bu şekilde de kullanılabilir.
 
 //Rate Limiting
 app.use("/api", rateLimit);
@@ -65,7 +51,6 @@ app.use(errorHandlerMiddleware);
 // );
 
 app.use("/api", router);
-
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/", (req, res) => {
